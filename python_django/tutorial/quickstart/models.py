8bd662e1e5a1d04d658d3c0955f8245394a29234
993506205 <993506205@qq.com>
2018-01-14 21:14:53 +0200

    Add files via upload

---
+++ python_django/tutorial/quickstart/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+	bookName = models.CharField(max_length=45)
+	authors = models.CharField(max_length=100)
+	publish_date = models.DateField()
+	
+	def __str__(self):
+		return self.bookName
---
+++ python_django/tutorial/quickstart/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User, Group
+from models import Book
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'groups')
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('url', 'name')
+
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('url', 'bookName', 'authors' , 'publish_date')
