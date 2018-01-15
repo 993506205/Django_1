8f09ceb5321ebbedb335048dbdeb2be911c3653a
993506205 <993506205@qq.com>
2018-01-15 19:12:58 +0200

    Add files via upload

--- python_django/tutorial/quickstart/admin.py
+++ python_django/tutorial/quickstart/admin.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 # Register your models here.
-from .models import Book
+from django.contrib.auth.models import Book
 
 admin.site.register(Book)
